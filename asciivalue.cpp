19f58028ed0e59b5f5f0f6399a41826c47e03031
Biswanath-2004 <biswanathchakraborty863@gmail.com>
2025-08-03 23:17:06 +0530

    Add files via upload

---
+++ asciivalue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char ch;  
+    // cin>>ch;
+    // int x = int (ch);(//char to int)
+    // cout<<x; 
+
+    int x;
+    cin>>x;
+    char ch =char (x);(//int to char)
+    cout<<ch;
+}
